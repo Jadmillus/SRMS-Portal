e620c450093f4f3b70ae93ae4d8cd04aec379274
Jadmillus <91867446+Jadmillus@users.noreply.github.com>
2023-11-21 20:41:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '370HSSV0773H'
-    SQLALCHEMY_DATABASE_URI = "mysql://root:icui4cu2@localhost/srms"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost/db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = "rogueoutis@gmail.com"
-    MAIL_PASSWORD = 'zkst nxom wqbx vwna' 
+    MAIL_USERNAME = ""
+    MAIL_PASSWORD = '' 

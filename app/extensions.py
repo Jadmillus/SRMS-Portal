9965cb43c913b7cad962c3608cfaebadc06ffbe4
Jadmillus <jadmillusorangi@gmail.com>
2023-11-21 20:25:09 +0300

    extensions

---
+++ app/extensions.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+mail = Mail()
+
+login_manager = LoginManager()
+
+login_manager.login_view = 'user.login'
+login_manager.login_message_category = 'info'
+
